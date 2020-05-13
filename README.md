ac52253a70ed7642e3ef1ab7d86cd22f05a303e9
Amber Outman <amberoutman@yahoo.com>
2020-05-12 19:13:20 -0700

    finished read me

---
+++ README.md
@@ -0,0 +1,52 @@
+# ReadME
+
+
+## Description
+
+ReadME generator 
+
+## Table of Contents
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+```
+npm i
+```
+
+## Usage
+
+Nothing
+
+## License
+
+This project is licensed under the MIT license.
+
+## Contributing
+
+Nothing x2
+
+## Tests
+
+```
+npm test
+```
+
+## Questions
+
+<img src="https://avatars1.githubusercontent.com/u/58788822?v=4" alt="avatar" style="border-radius: 16px" width="30"/>
+
+If you have any questions, open an issue or contact [AmberOutman](https://github.com/AmberOutman) directly at null.
+
+
--- index.js
+++ index.js
@@ -4,15 +4,49 @@ const readline = require("readline").createInterface({
 });
 
 const axios = require("axios");
-
+const fs = require("fs");
 const readUsername = () => {
     readline.question("Enter your GitHub username: ", username => {
         axios.get("https://api.github.com/users/" + username)
             .then(response => {
                 const profile = response.data;
                 readline.question("Enter the title of your project: ", title => {
-                    readline.question("Enter Description", description =>{
-
+                    readline.question("Enter Description: ", description =>{
+                        readline.question("Enter project license: ", license =>{
+                            readline.question("Enter command to install dependencies: ", dependencyCommand =>{
+                                readline.question("Enter command to run test: ", testCommand =>{
+                                    readline.question("What does the user need to know about using the repo? ", usage => {
+                                        readline.question("What does the user need to know about contributing to the repo? ", contributing =>{
+                                            let fileData = ""; 
+                                            fileData += "# " + title +"\n\n\n";
+                                            fileData += "## Description\n\n" + description + "\n\n";
+                                            fileData += "## Table of Contents\n\n";
+                                            fileData += "* [Installation](#installation)\n\n";   
+                                            fileData += "* [Usage](#usage)\n\n";   
+                                            fileData += "* [License](#license)\n\n";   
+                                            fileData += "* [Contributing](#contributing)\n\n";   
+                                            fileData += "* [Tests](#tests)\n\n";   
+                                            fileData += "* [Questions](#questions)\n\n";   
+                                            fileData += "## Installation\n\n```\n" + dependencyCommand + "\n```\n\n";
+                                            fileData += "## Usage\n\n" + usage + "\n\n";
+                                            fileData += "## License\n\nThis project is licensed under the " + license + " license.\n\n";
+                                            fileData += "## Contributing\n\n" + contributing + "\n\n";
+                                            fileData += "## Tests\n\n```\n" + testCommand + "\n```\n\n";
+                                            fileData += "## Questions\n\n";
+                                            fileData += "<img src=\""+ profile.avatar_url + "\" alt=\"avatar\" style=\"border-radius: 16px\" width=\"30\"/>\n\n";
+                                            fileData += "If you have any questions, open an issue or contact [" + profile.login + "](" + profile.html_url + ") directly at " + profile.email + ".\n\n\n";
+                                            fs.writeFile("README.md", fileData, err => {
+                                                if(err){
+                                                    console.log(error);
+                                                }
+                                                readline.close();
+                                                process.exit();
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
                     });
                 });
             })
